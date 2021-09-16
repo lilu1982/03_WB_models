b8adb78694b08c18b25202b08b478494fff76d9a
Peter <schmidt.multimedia@gmail.com>
2021-09-16 12:54:29 +0200

    update 1-1

---
+++ 02_kardinal_1-1/sql/01_tab_unverbunden.sql
@@ -0,0 +1,17 @@
+-- 1. Tabellen, unverbunden
+
+-- Tab cats
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(45) NOT NULL,
+  `fur_color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- Tab servants
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servant_name` VARCHAR(45) NOT NULL,
+  `yrs_served` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
