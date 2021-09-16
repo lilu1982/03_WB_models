4fa295d121d0fcb7040747a9e5eb356fdbbf1320
Daniel <homenetmedia82@gmail.com>
2021-09-16 09:51:12 +0200

    update
    update

---
+++ SQL/01_tab_cats.sql
@@ -0,0 +1,13 @@
+-- Anlegen der Tabelle
+
+CREATE TABLE IF NOT EXISTS `mydb´.`cats` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(45) NOT NULL,
+`age` INT NOT NULL,
+PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+INSERT INTO `mydb`.`cats` (`name`, `age`) VALUES ( "Alonzo", 35);
+INSERT INTO `mydb`.`cats` (`name`, `age`) VALUES ( "Grizabella", 29);
+
+SELECT * FROM cats;
